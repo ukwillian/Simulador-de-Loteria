8bd65dde0db4489f4af2559934280ed7e9051a13
Willian Soares <uk.willian@gmail.com>
2025-07-20 15:13:50 -0400

    1

---
+++ app.js
@@ -0,0 +1,58 @@
+function sortear(){
+    let quantidade = parseInt(document.getElementById('quantidade').value);
+    let de = parseInt(document.getElementById('de').value);
+    let ate = parseInt(document.getElementById('ate').value);
+
+    if (de >= ate) {
+        alert ('O número inicial precisa ser menor que o número final.');
+        return;
+    }
+    
+    if (quantidade > (ate - de + 1)) {
+    alert('Campo "Quantidade" deve ser menor ou igual ao intervalo informado no campo "Do número" até o campo "Até o número". Verifique!');
+    return;
+  }
+
+    let sorteados = [];
+    let numero;
+
+    for (let i = 0; i <quantidade; i++) {
+        numero = obterNumeroAleatorio(de, ate);
+        
+        while(sorteados.includes(numero)) {
+        numero = obterNumeroAleatorio(de,ate);
+              alert('Tentando obter número inédito');
+    }
+
+     sorteados.push(numero);
+    }
+
+    sorteados.sort((a, b) => a - b);
+
+    let retultado = document.getElementById('Resultado');
+    resultado.innerHTML = `<label class="texto__paragrafo">Números sorteados:  ${sorteados} </label>`
+    alterarStatusBotao();
+}
+
+function obterNumeroAleatorio(min, max) {
+    return Math.floor(Math.random() * (max - min) + min);
+}
+
+function alterarStatusBotao() {
+    let botao = document.getElementById('btn-reiniciar');
+    if (botao.classList.contains('container__botao-desabilitado')){
+        botao.classList.remove('container__botao-desabilitado');
+        botao.classList.add('container__botao');
+    } else {
+        botao.classList.remove('container__botao');
+        botao.classList.add('container__botao-desabilitado');
+    }
+}
+
+function reiniciar() {
+    document.getElementById('quantidade').value = '';
+    document.getElementById('de').value = '';
+    document.getElementById('ate').value = '';
+    document.getElementById('resultado').innerHTML = '<label class="texto__paragrafo">Números sorteados:  nenhum até agora</label>';
+    alterarStatusBotao();
+}
